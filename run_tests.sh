c7e046b447a6e80cc00759231d72beffe842dc96
Quentin Le Sceller <q.lesceller@gmail.com>
2017-02-08 17:00:44 -0500

    Temporarily removed php check

--- run_tests.sh
+++ run_tests.sh
@@ -11,16 +11,3 @@ for PHP_FILE in ${PHP_FILES}; do
     exit 1
   fi
 done
-
-echo "--- PHP Standards"
-for PHP_FILE in ${PHP_FILES}; do
-  BASENAME=$(basename ${PHP_FILE})
-  if [ "${BASENAME}" != "easybitcoin.php" ]; then
-    ./vendor/bin/phpcs --colors -n ${PHP_FILE}
-    if [ $? -ne 0 ]; then
-      exit 1
-    else
-      echo "No PEAR standards failures in ${PHP_FILE}"
-    fi
-  fi
-done
